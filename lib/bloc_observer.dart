832e04198a15d39a2186582c79f6db7100f30a7e
Maneesh Kumar <maneesh.kumar_cs18@gla.ac.in>
2021-04-20 14:42:04 +0530

    Delete lib directory

--- lib/bloc_observer.dart
+++
@@ -1,9 +0,0 @@
-import 'package:bloc/bloc.dart';
-
-class SimpleBlocObserver extends BlocObserver {
-  @override
-  void onTransition(Bloc bloc, Transition transition) {
-    print(transition);
-    super.onTransition(bloc, transition);
-  }
-}
